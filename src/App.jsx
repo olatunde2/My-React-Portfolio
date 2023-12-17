05fab361c43a518e13b1c0f6bf6833cacdb59537
olatunde2 <conolthomas2@gmail.com>
2023-12-17 17:44:25 +0300

    correct contact commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
 import Services from "./components/services/Services";
 import PortFolio from "./components/portfolio/PortFolio";
-import Testimonials from "./components/testimonials/Testimonials";
+// import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 
@@ -18,7 +18,7 @@ const App = () => {
       <Experience />
       <Services />
       <PortFolio />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Contact />
       <Footer />
     </>
